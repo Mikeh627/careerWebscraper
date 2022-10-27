88b0150429f9232262e876510b8ee0d797f02798
Bailey Lewis <Bdlewis5243@eagle.fgcu.edu>
2022-10-27 11:29:20 -0400

    Added information in the header

--- main.py
+++ main.py
@@ -5,6 +5,14 @@
 # A webscraper to take in information and to upload them to a local database,
 #   to then finally be uploaded to a website.
 
+# Links:
+# LinkedIn Software Engineering Job Listings.
+#   Format:: Company: Position: Location
+#   https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=United%20States&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0
+
+# Microsoft LinkedIn Job Listings:
+#   Format:: Company: Position: Location: Pay
+#   https://www.linkedin.com/jobs/microsoft-software-engineer-jobs?position=1&pageNum=0
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
