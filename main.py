6fe832d638975c21450056624e8c3babb30410bd
Mike <mrhouseholder8288@eagle.fgcu.edu>
2022-10-27 14:15:21 -0400

    fleshing out webscrapper on job listing sites.

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-# Webscraper for Networking Project
-# CNT 4104
-# Bailey Lewis, Michael Householder, Joshua Cowin
+#   Webscraper for Networking Project
+#   CNT 4104
+#   Bailey Lewis, Michael Householder, Joshua Cowin
 
-# A webscraper to take in information and to upload them to a local database,
+#   A webscraper to take in information and to upload them to a local database,
 #   to then finally be uploaded to a website.
 
-# Links:
-# LinkedIn Software Engineering Job Listings.
+#   Links:
+#   LinkedIn Software Engineering Job Listings.
 #   Format:: Company: Position: Location
 #   https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=United%20States&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0
 
-# Microsoft LinkedIn Job Listings:
+#   Microsoft LinkedIn Job Listings:
 #   Format:: Company: Position: Location: Pay
 #   https://www.linkedin.com/jobs/microsoft-software-engineer-jobs?position=1&pageNum=0
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+from bs4 import BeautifulSoup
+import requests
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+job_request = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=software+engineering&txtLocation=united+states').text
+
+soup = BeautifulSoup(job_request, 'lxml')
+jobsList = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
 
-print_hi('bad team!')
+print(jobsList)
+
