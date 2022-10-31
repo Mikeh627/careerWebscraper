8bbe215de9b15c4508eb69aacf0ac57e9f42266a
Bailey Lewis <bdlewis5243@eagle.fgcu.edu>
2022-10-31 13:06:28 -0400

    Finally got requests working and now we can search websites

--- main.py
+++ main.py
@@ -16,13 +16,49 @@
 
 from bs4 import BeautifulSoup
 import requests
+from requests.exceptions import HTTPError
+import sys
 
+linksFromSearch = {}
+# sys allows us to grab arguments.
+# to install Beautiful Soup: File-->Settings-->Project Interpreter and "add package"
 
-job_request = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=software+engineering&txtLocation=united+states').text
+# ----------REALLY BIG NOTE: When using terminal don't use Python3 just use py when doing commands----------
+# response.content = content returned from the browser
+# response.text = How requests changes it over to a string
 
-soup = BeautifulSoup(job_request, 'lxml')
-jobsList = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+if __name__ == '__main__':
+    for url in sys.argv[1:]:
+        # This will add https:// to the beginning of our links when we begin our search with 'www.'
+        if not url.lower().startswith('http'):
+            url = f'https://{url}'
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
 
+        # Using HTTP error allows us to get the status code and its description:
+        #   As in 404: Not found
+        except HTTPError as httperr:
+            print(f'HTTP error: {httperr}')
+            sys.exit(1)
+        except Exception as err:
+            print(f'Something went really wrong!: {err}')
+            sys.exit(1)
 
-print(jobsList)
+        # Open our page with beautiful soup to parse it and find information
+        soup = BeautifulSoup(response.text, 'html.parser')
 
+        # For Companies
+        for link in soup.findAll("a", class_='hidden-nested-link'):
+            print("wow!")
+        # For Location
+        for link in soup.findAll("span", class_='job-search-card__location'):
+            text = link.string
+            text.replace('\n', '')
+            text.replace(' ', '')
+            text.strip()
+            with open("results.txt", "a") as namesfile:
+                namesfile.write(text)
+            print(text)
+
+            # Command:  py main.py 'https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=United%20States&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
