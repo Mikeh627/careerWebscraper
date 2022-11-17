16af955171ef24fa3aee153964125dbc8bfd907d
Bailey Lewis <Bdlewis5243@eagle.fgcu.edu>
2022-11-17 09:52:31 -0500

    Got linkedIn working for basic software engineer searching in the U.S.

--- main.py
+++ main.py
@@ -25,66 +25,57 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 
 
-listOfCompanies = {}
-listOfLocations = {}
+listOfCompanies = []
+listOfLocations = []
+listOfJobTitles = []
 
+def makeRows(row):
+    words = ""
+
+    for i, letter in enumerate(row):
+        if not letter.isdigit():
+            words += letter
+        else:
+            return [words.strip(), *row[i:].split(" ")]
 def getLinkedIn():
-    driver = webdriver.Firefox()
+    # Loading in chrome but it needs to be in incognito or you will need to be signed in or create a automated process
+    # to sign in
+    driver = webdriver.ChromeOptions()
+    driver.add_argument("--incognito")
+    driver = webdriver.Chrome(chrome_options=driver)
     driver.get("https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=United%20States&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0")
     assert "Page not found" not in driver.page_source
+
     # XPath for Job title: Increment N starting from 1 to N
     # //*[@id="main-content"]/section[2]/ul/li[N]/div/div[2]/h3
-    title = 'title'
+    for row in range(1, 5):
+        rows = driver.find_elements(By.XPATH, "//*[@id='main-content']/section[2]/ul/li[" + str(row) + "]/div/div[2]/h3")
+        for row_data in rows:
+            #print(f"\n=====row_data {row_data.text}=====\n")
+            listOfJobTitles.append(row_data.text)
+
+    print(listOfJobTitles)
 
     # XPath for company: Increment N starting from 1 to N
     # //*[@id="main-content"]/section[2]/ul/li[N]/div/div[2]/h4/a
-    company = 'company'
+    for row in range(1, 5):
+        rows = driver.find_elements(By.XPATH, "//*[@id='main-content']/section[2]/ul/li[" + str(row) + "]/div/div[2]/h4")
+        for row_data in rows:
+            #print(f"\n=====row_data {row_data.text}=====\n")
+            listOfCompanies.append(row_data.text)
+
+    print(listOfCompanies)
 
     # Xpath for Location: Increment N starting from 1 to N
     # //*[@id="main-content"]/section[2]/ul/li[N]/div/div[2]/div/span
-    location = 'location'
+    for row in range(1, 5):
+        rows = driver.find_elements(By.XPATH, "//*[@id='main-content']/section[2]/ul/li[" + str(row) + "]/div/div[2]/div/span")
+        for row_data in rows:
+            #print(f"\n=====row_data {row_data.text}=====\n")
+            listOfLocations.append(row_data.text)
 
-    return (company, title, location)
+    driver.close()
+    print(listOfLocations)
 
 if __name__ == '__main__':
-    for url in sys.argv[1:]:
-        # This will add https:// to the beginning of our links when we begin our search with 'www.'
-        if not url.lower().startswith('http'):
-            url = f'https://{url}'
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-
-        # Using HTTP error allows us to get the status code and its description:
-        #   As in 404: Not found
-        except HTTPError as httperr:
-            print(f'HTTP error: {httperr}')
-            sys.exit(1)
-        except Exception as err:
-            print(f'Something went really wrong!: {err}')
-            sys.exit(1)
-
-        # Open our page with beautiful soup to parse it and find information
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # For Companies
-        for link in soup.findAll("a", class_='hidden-nested-link'):
-            location = link.string
-            with open("companies.txt", "w") as namesfile:
-                namesfile.write(location)
-                x = 0
-                listOfLocations[x] = location
-                x += 1
-        # For Location
-        for link in soup.findAll("span", class_='job-search-card__location'):
-            location = link.string
-            with open("locations.txt", "w") as namesfile:
-                namesfile.write(location)
-                x = 0
-                listOfLocations[x] = location
-                x += 1
-
-
-
-
-            # Command:  py main.py ''
+    getLinkedIn()
