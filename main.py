c6cfa3710d6cf385b9742b657863df513b501426
Bailey Lewis <Bdlewis5243@eagle.fgcu.edu>
2022-11-17 10:13:53 -0500

    Finished Microsoft Job listings

--- main.py
+++ main.py
@@ -12,11 +12,12 @@
 
 #   Microsoft LinkedIn Job Listings:
 #   Format:: Company: Position: Location: Pay
-#   https://www.linkedin.com/jobs/microsoft-software-engineer-jobs?position=1&pageNum=0
+#   https://www.linkedin.com/jobs/search?keywords=Microsoft&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0
 
 # Blocking warning because its annoying
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 #-----Selenium-----
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
@@ -27,14 +28,11 @@ listOfCompanies = []
 listOfLocations = []
 listOfJobTitles = []
 
-def makeRows(row):
-    words = ""
+listOfMicroCompanies = []
+listOfMicroLocations = []
+listOfMicroJobTitles = []
 
-    for i, letter in enumerate(row):
-        if not letter.isdigit():
-            words += letter
-        else:
-            return [words.strip(), *row[i:].split(" ")]
+# Searching for Software Engineer Job listings in the U.S
 def getLinkedIn():
     # Loading in chrome but it needs to be in incognito or you will need to be signed in or create a automated process
     # to sign in
@@ -75,5 +73,52 @@ def getLinkedIn():
     driver.close()
     print(listOfLocations)
 
+# Searching for Microsoft Job listings in the U.S.
+def getLinkedInMircosoft():
+    # Loading in chrome but it needs to be in incognito or you will need to be signed in or create a automated process
+    # to sign in
+    driver = webdriver.ChromeOptions()
+    driver.add_argument("--incognito")
+    driver = webdriver.Chrome(chrome_options=driver)
+    driver.get(
+        "https://www.linkedin.com/jobs/search?keywords=Microsoft&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0")
+    assert "Page not found" not in driver.page_source
+
+    # XPath for Job title: Increment N starting from 1 to N
+    # //*[@id="main-content"]/section[2]/ul/li[N]/div/div[2]/h3
+    for row in range(1, 5):
+        rows = driver.find_elements(By.XPATH,
+                                    "//*[@id='main-content']/section[2]/ul/li[" + str(row) + "]/div/div[2]/h3")
+        for row_data in rows:
+            # print(f"\n=====row_data {row_data.text}=====\n")
+            listOfMicroJobTitles.append(row_data.text)
+
+    print(listOfMicroJobTitles)
+
+    # XPath for company: Increment N starting from 1 to N
+    # //*[@id="main-content"]/section[2]/ul/li[N]/div/div[2]/h4/a
+    for row in range(1, 5):
+        rows = driver.find_elements(By.XPATH,
+                                    "//*[@id='main-content']/section[2]/ul/li[" + str(row) + "]/div/div[2]/h4")
+        for row_data in rows:
+            # print(f"\n=====row_data {row_data.text}=====\n")
+            listOfMicroCompanies.append(row_data.text)
+
+    print(listOfMicroCompanies)
+
+    # Xpath for Location: Increment N starting from 1 to N
+    # //*[@id="main-content"]/section[2]/ul/li[N]/div/div[2]/div/span
+    for row in range(1, 5):
+        rows = driver.find_elements(By.XPATH,
+                                    "//*[@id='main-content']/section[2]/ul/li[" + str(row) + "]/div/div[2]/div/span")
+        for row_data in rows:
+            # print(f"\n=====row_data {row_data.text}=====\n")
+            listOfMicroLocations.append(row_data.text)
+
+    driver.close()
+    print(listOfMicroLocations)
+
 if __name__ == '__main__':
     getLinkedIn()
+    print("------------------Loading next function------------------")
+    getLinkedInMircosoft()
